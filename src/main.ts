e269b71e3dd29fbe24b7eff44b2094d63108b2a5
Paulo Becker <beckerpaulo81@gmail.com>
2026-06-02 10:15:26 -0300

    Projeto iniciado

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+/* eslint-disable prettier/prettier */
+function main(a: number, b: number): number {
+  
+    return a / b;
+}
+
+//const result = main(2, 3);
+console.log(main(2, 3));
+//console.log(`The result is: ${result}`);
